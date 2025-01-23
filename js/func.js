8a067e221bf5d2fd6d7aefc9c85bce8951a6a9f6
Marcos Ribeiro <marcsfic98@gmail.com>
2025-01-23 17:53:30 -0300

    finalizaçao do projeto !!

---
+++ js/func.js
@@ -0,0 +1,23 @@
+$(function(){
+    $("nav.mobile i").click(function(){
+        var el = $(this).parent().find('ul');
+        if(el.is(':visible')==false){
+            el.fadeIn();
+        }else{
+            el.fadeOut();
+        }
+    })
+
+
+    $('nav a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $(html , body).animate({'scrollTop':offSetTop});
+
+        return false;
+    })
+
+})
+
+
